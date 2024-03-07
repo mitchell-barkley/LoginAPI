ec16ef3fdbd2744714f3d9b6ef1686465a31c66a
mitchell-barkley <138683597+mitchell-barkley@users.noreply.github.com>
2024-03-07 16:20:13 -0330

    Initialized ejs and server with basic page layout

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const methodOverride = require('method-override');
+const app = express();
+const port = 3000;
+
+global.DEBUG = true;
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', { name: 'Mitchell' });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+app.use((req, res) => {
+    res.status(404).render('404.ejs');
+});
