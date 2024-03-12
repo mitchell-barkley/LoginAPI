e406d1a3d3cd7e6177fef0c205955de1b140c7c5
mitchell-barkley <138683597+mitchell-barkley@users.noreply.github.com>
2024-03-12 20:47:14 -0230

    update and delete functionality

--- routes/logins.js
+++ routes/logins.js
@@ -3,11 +3,6 @@ const router = express.Router();
 const loginsDal = require('../services/pg.logins.dal')
 
 router.get('/', async (req, res) => {
-//   const theLogins = [
-//       {id: 1, username: 'example', password: 'example'},
-//       {id: 4, username: 'frodob', password: 'example'},
-//       {id: 7, username: 'bilbob', password: 'example'}
-//   ];
     try {
         let theLogins = await loginsDal.getLogins();
         if(DEBUG) console.table(theLogins);
@@ -17,10 +12,55 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        let aLogin = await loginsDal.getLoginById(req.params.id);
+        if(DEBUG) console.table(aLogin);
+        if(aLogin.length === 0){
+            res.render('norecord', {id: req.params.id, type: 'login'});
+        }
+        res.render('login', {aLogin});
+    } catch {
+        res.render('503');
+    }
+});
+
 router.post('/', async (req, res) => {
     if(DEBUG) console.log("logins.POST");
     try {
         await loginsDal.addLogin(req.body.username, req.body.password);
+        res.redirect('/logins/');
+    } catch (err){
+        if(DEBUG) console.log(err);
+        res.render('503');
+    }
+});
+
+router.get('/:id/edit', async (req, res) => {
+    if(DEBUG) console.log("logins.EDIT");
+    res.render('loginPatch.ejs', {username: req.query.username, theId: req.params.id});
+});
+
+router.get('/:id/delete', async (req, res) => {
+    if(DEBUG) console.log("logins.DELETE");
+    res.render('loginDelete.ejs', {username: req.query.username, theId: req.params.id});
+});
+
+router.patch('/:id', async (req, res) => {
+    if(DEBUG) console.log("logins.PATCH");
+    try {
+        await loginsDal.updateLogin(req.params.id, req.body.username, req.body.password);
+        res.redirect('/logins/');
+    } catch (err){
+        if(DEBUG) console.log(err);
+        res.render('503');
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    if(DEBUG) console.log("logins.DELETE");
+    try {
+        await loginsDal.deleteLogin(req.params.id);
         res.redirect('/logins/');
     } catch (err){
         if(DEBUG) console.log(err);
--- services/pg.logins.dal.js
+++ services/pg.logins.dal.js
@@ -1,21 +1,5 @@
 const dal = require("./auth_db");
 
-var addLogin = function (username, password) {
-    if(DEBUG) console.log('services/pg.logins.dal.addLogin - called');
-    return new Promise((resolve, reject) => {
-        const sql = `INSERT INTO public."Logins" (username, password) \
-            VALUES ($1, $2)`;
-        dal.query(sql, [username, password], (err, res) => {
-            if (err) {
-                if(DEBUG) console.log(err);
-                reject(err);
-            } else {
-                resolve(result.rows);
-            }
-        });
-    });
-};
-
 var getLogins = function () {
     if(DEBUG) console.log('services/pg.logins.dal.getLogin - called');
     return new Promise((resolve, reject) => {
@@ -31,7 +15,71 @@ var getLogins = function () {
     });
 };
 
+var getLoginById = function (id) {
+    if(DEBUG) console.log('services/pg.logins.dal.getLoginById - called');
+    return new Promise((resolve, reject) => {
+        const sql = `SELECT * FROM public."Logins" WHERE id = $1;`;
+        dal.query(sql, [id], (err, res) => {
+            if (err) {
+                if(DEBUG) console.log(err);
+                reject(err);
+            } else {
+                resolve(res.rows);
+            }
+        });
+    });
+};
+
+var addLogin = function (username, password) {
+    if(DEBUG) console.log('services/pg.logins.dal.addLogin - called');
+    return new Promise((resolve, reject) => {
+        const sql = `INSERT INTO public."Logins" (username, password) \
+            VALUES ($1, $2)`;
+        dal.query(sql, [username, password], (err, res) => {
+            if (err) {
+                if(DEBUG) console.log(err);
+                reject(err);
+            } else {
+                resolve(res.rows);
+            }
+        });
+    });
+};
+
+var updateLogin = function(id, username, password) {
+    if(DEBUG) console.log('logins.pg.dal.updateLogin - called');
+    return new Promise((resolve, reject) => {
+        const sql = `UPDATE public."Logins" SET username=$2, password=$3 WHERE id=$1;`;
+        dal.query(sql, [id, username, password], (err, res) => {
+            if (err) {
+                if(DEBUG) console.log(err);
+                reject(err);
+            } else {
+                resolve(res.rows);
+            }
+        });
+    });
+};
+
+var deleteLogin = function(id) {
+    if(DEBUG) console.log('services/pg.logins.dal.deleteLogin - called');
+    return new Promise((resolve, reject) => {
+        const sql = `DELETE FROM public."Logins" WHERE id = $1;`;
+        dal.query(sql, [id], (err, res) => {
+            if (err) {
+                if(DEBUG) console.log(err);
+                reject(err);
+            } else {
+                resolve(res.rows);
+            }
+        });
+    });
+};
+
 module.exports = {
     addLogin,
-    getLogins
+    getLogins,
+    getLoginById,
+    updateLogin,
+    deleteLogin
 };
