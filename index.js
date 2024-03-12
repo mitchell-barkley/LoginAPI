a9e3fa7a49dd1d9812f52b182ee53d04a366ea88
mitchell-barkley <138683597+mitchell-barkley@users.noreply.github.com>
2024-03-12 17:58:23 -0230

    fixed redirect from nav links

--- index.js
+++ index.js
@@ -9,21 +9,20 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-const apiRouter = require('./routes/api');
-app.use('/api', apiRouter);
-
 app.get('/', (req, res) => {
     res.render('./index.ejs', { name: 'Mitchell' });
 });
 
-app.get('/login', (req, res) => {
-    res.render('./login.ejs');
+const loginsRouter = require('./routes/logins');
+app.use('/logins', loginsRouter);
+
+const apiRouter = require('./routes/api');
+app.use('/api', apiRouter);
+
+app.use((req, res) => {
+    res.status(404).render('404.ejs');
 });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-});
-
-app.use((req, res) => {
-    res.status(404).render('404.ejs');
 });
--- routes/api/logins.js
+++ routes/api/logins.js
@@ -3,20 +3,10 @@ const loginsDal = require('../../services/pg.logins.dal.js');
 
 if (DEBUG) console.log('API - logins.js - called');
 
-router.post('/', async (req, res) => {
-    if (DEBUG) console.log('routes/api/logins.js - POST / - called');
-    try {
-        const result = await loginsDal.addLogin(req.body.username, req.body.password);
-        res.send(result);
-    } catch (err) {
-        res.status(500).send(err);
-    }
-});
-
 router.get('/', async (req, res) => {
     if (DEBUG) console.log('routes/api/logins.js - GET / - called');
     try {
-        const theLogins = await loginsDal.getLogin();
+        let theLogins = await loginsDal.getLogins();
         res.json(theLogins);
     } catch {
         res.statusCode = 503;
